4b389d2834dec6a092f5befae266debf2ec99cc4
kevin <kevingemolo@gmail.com>
2020-03-21 20:43:01 +0100

    Notifiche sensori

--- php/src/api/Check.php
+++ php/src/api/Check.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace HomeSensors\api;
 
 use HomeSensors\Page;
+use HomeSensors\PushUtils;
 use Rakit\Validation\Validation;
 use Rakit\Validation\Validator;
 
@@ -14,18 +15,54 @@ class Check extends Page {
     }
 
     protected function exec() {
-        echo "ciaociao";
-//        PushUtils::getClient()->publishToInterests(
-//            ["sensori"],
-//            [
-//                "fcm" => [
-//                    "notification" => [
-//                        "title" => "Prova",
-//                        "body" => "Notifica di prova"
-//                    ],
-//                ],
-//            ],
-//            );
-//        echo "notifica inviata";
+
+        if ($curl = curl_init('gpio:5000')) {
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($curl);
+            $obj = (json_decode($response));
+
+        }
+        if ($obj->fuoco === true) {
+            PushUtils::getClient()->publishToInterests(
+                ["sensori"],
+                [
+                    "fcm" => [
+                        "notification" => [
+                            "title" => "Fuoco",
+                            "body"  => "Stai andando a fuoco",
+                        ],
+                    ],
+                ],
+                );
+            echo "notifica inviata";
+        }
+        if ($obj->gas === true) {
+            PushUtils::getClient()->publishToInterests(
+                ["sensori"],
+                [
+                    "fcm" => [
+                        "notification" => [
+                            "title" => "Gas",
+                            "body"  => "C'è del gas in casa",
+                        ],
+                    ],
+                ],
+                );
+            echo "notifica inviata";
+        }
+        if ($obj->pioggia === true) {
+            PushUtils::getClient()->publishToInterests(
+                ["sensori"],
+                [
+                    "fcm" => [
+                        "notification" => [
+                            "title" => "Pioggia",
+                            "body"  => "Sta piovendo",
+                        ],
+                    ],
+                ],
+                );
+            echo "notifica inviata";
+        }
     }
 }
